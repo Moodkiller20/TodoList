a2c888b973e079ef899e064d8b88e517f880ad9b
Giovanni Tshiabngu <61130107+Moodkiller20@users.noreply.github.com>
2022-11-24 22:57:47 -0500

    new features

--- HW4_TodoList/settings.py
+++ HW4_TodoList/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -84,7 +83,7 @@ WSGI_APPLICATION = 'HW4_TodoList.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('DATABASE_NAME'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
